a21d6f250ac25846f5e4e20a8cd7ef60de7e08dc
Konstantin Varvarkin <varvarkin.k@gmail.com>
2023-10-18 01:37:00 +0300

    Добавил решение второго задания

--- qa_guru_python_8_10/pages/registration_page.py
+++ qa_guru_python_8_10/pages/registration_page.py
@@ -6,62 +6,43 @@ class RegistrationPage:
     def open(self):
         browser.open('/automation-practice-form')
 
-    def fill_first_name(self, value):
-        browser.element('#firstName').should(be.visible).type(value)
-
-    def fill_last_name(self, value):
-        browser.element('#lastName').should(be.visible).type(value)
-
-    def fill_email(self, value):
-        browser.element('#userEmail').should(be.visible).type(value)
-
-    def choose_a_gender(self):
-        browser.element('[for=gender-radio-1]').should(be.visible).click()
-
-    def fill_telephone_number(self, value):
-        browser.element('#userNumber').should(be.visible).type(value)
-
-    def choose_date_of_birth(self, month, year, day):
+    def register(self, user):
+        browser.element('#firstName').should(be.visible).type(user.first_name)
+        browser.element('#lastName').should(be.visible).type(user.last_name)
+        browser.element('#userEmail').should(be.visible).type(user.email)
+        browser.all('.custom-radio').element_by(have.text(user.gender)).click()
+        browser.element('#userNumber').should(be.visible).type(user.phone_number)
         browser.element('#dateOfBirthInput').should(be.visible).click()
         browser.element('.react-datepicker__month-select').should(be.visible).click()
-        browser.element(f'.react-datepicker__month-select > option:nth-child({month})').should(be.visible).click()
+        browser.element(f'.react-datepicker__month-select > option:nth-child({user.month_of_brith})').should(be.visible).click()
         browser.element('.react-datepicker__year-select').should(be.visible).click()
-        browser.element(f'.react-datepicker__year-select > option:nth-child({year})').should(be.visible).click()
-        browser.element(f'.react-datepicker__day.react-datepicker__day--{day}').should(be.visible).click()
-
-    def choose_a_subject(self, value):
-        browser.element('#subjectsInput').should(be.visible).type(value).press_enter()
-
-    def choose_a_hobby(self):
-        browser.element('label[for=hobbies-checkbox-1]').should(be.visible).click()
-        browser.element('label[for=hobbies-checkbox-2]').should(be.visible).click()
-        browser.element('label[for=hobbies-checkbox-3]').should(be.visible).click()
-
-    def upload_a_picture(self, value):
-        browser.element('#uploadPicture').should(be.visible).type(resource.path(value))
-
-    def type_current_address(self, value):
-        browser.element('#currentAddress').should(be.visible).type(value)
-
-    def choose_state(self, value):
-        browser.element("#react-select-3-input").should(be.visible).type(value).press_enter()
-
-    def choose_city(self, value):
-        browser.element("#react-select-4-input").should(be.visible).type(value).press_enter()
-
-    def submit_form(self):
+        browser.element(f'.react-datepicker__year-select > option:nth-child({user.year_of_brith})').should(be.visible).click()
+        browser.element(f'.react-datepicker__day.react-datepicker__day--{user.day_of_brith}').should(be.visible).click()
+        browser.element('#subjectsInput').should(be.visible).type(user.subject).press_enter()
+        if 'Sports' in user.hobby:
+            browser.element('label[for=hobbies-checkbox-1]').should(be.visible).click()
+        if 'Reading' in user.hobby:
+            browser.element('label[for=hobbies-checkbox-2]').should(be.visible).click()
+        if 'Music' in user.hobby:
+            browser.element('label[for=hobbies-checkbox-3]').should(be.visible).click()
+        browser.element('#uploadPicture').should(be.visible).type(resource.path(user.picture))
+        browser.element('#currentAddress').should(be.visible).type(user.current_address)
+        browser.element("#react-select-3-input").should(be.visible).type(user.state).press_enter()
+        browser.element("#react-select-4-input").should(be.visible).type(user.city).press_enter()
         browser.element("#submit").should(be.visible).click()
 
-    def student_should_by_registred(self, full_name, email, gender, phone_number, date_of_birth, subject, hobby, picture, state, city):
+    def student_should_by_registred(self, user):
         browser.all(".table-dark>tbody>tr>td:nth-child(2)").should(have.texts(
-            full_name,
-            email,
-            gender,
-            phone_number,
-            date_of_birth,
-            subject,
-            hobby,
-            picture,
-            state,
-            city
+            f'{user.first_name} {user.last_name}',
+            user.email,
+            user.gender,
+            user.phone_number,
+            f"{user.day_of_brith.replace('0', '')} "
+            f"{user.month_of_brith.replace('5', 'May')},"
+            f"{user.year_of_brith.replace('20', '1919')}",
+            user.subject,
+            user.hobby,
+            user.picture,
+            user.current_address,
+            f'{user.state} {user.city}'
         ))
--- tests/test_student_should_be_registered.py
+++ tests/test_student_should_be_registered.py
@@ -1,37 +1,19 @@
 from qa_guru_python_8_10.pages.registration_page import RegistrationPage
+from qa_guru_python_8_10.data.users import User
 
 registration_page = RegistrationPage()
 
 
 def test_student_registration_form():
     # GIVEN
+    student = User(first_name='Vasya', last_name='Vasilyev', email='example@example.com', gender='Male', phone_number='7999999999',
+                   month_of_brith='5', year_of_brith='20', day_of_brith='012', subject='Computer Science',
+                   hobby='Sports, Reading, Music', picture='avatar.png', current_address='Palace Square, 2, St Petersburg, 190000', state='NCR',
+                   city='Delhi')
     registration_page.open()
 
     # WHEN
-    registration_page.fill_first_name('Vasya')
-    registration_page.fill_last_name('Vasilyev')
-    registration_page.fill_email('example@example.com')
-    registration_page.choose_a_gender()
-    registration_page.fill_telephone_number('7999999999')
-    registration_page.choose_date_of_birth(month='5', year='20', day='012')
-    registration_page.choose_a_subject('Computer Science')
-    registration_page.choose_a_hobby()
-    registration_page.upload_a_picture('avatar.png')
-    registration_page.type_current_address('Palace Square, 2, St Petersburg, 190000')
-    registration_page.choose_state('NCR')
-    registration_page.choose_city('Delhi')
-    registration_page.submit_form()
+    registration_page.register(student)
 
     # THEN
-    registration_page.student_should_by_registred(
-        'Vasya Vasilyev',
-        'example@example.com',
-        'Male',
-        '7999999999',
-        '12 May,1919',
-        'Computer Science',
-        'Sports, Reading, Music',
-        'avatar.png',
-        'Palace Square, 2, St Petersburg, 190000',
-        'NCR Delhi'
-    )
+    registration_page.student_should_by_registred(student)
