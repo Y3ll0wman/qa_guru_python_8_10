d515a35acc52648f555df78801d70446d05f97d5
Konstantin Varvarkin <varvarkin.k@gmail.com>
2023-10-17 23:28:37 +0300

    Переместил картинку и дописал resource.py

--- qa_guru_python_8_10/resource.py
+++ qa_guru_python_8_10/resource.py
@@ -1,7 +1,5 @@
 from pathlib import Path
 
-import tests
-
 
 def path(file_name):
-    return str(Path(tests.__file__).parent.joinpath(f"pictures/{file_name}"))
+    return str(Path(__file__).parent.joinpath(f"pictures/{file_name}"))
